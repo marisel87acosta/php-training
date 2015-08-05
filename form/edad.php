0789c325c08bd2ce8883a1ad8d080351ef4a015f
facundoledesma <facundo.ledesma95@gmail.com>
2015-08-05 10:28:40 -0300

    ejercicio de clase

--- form/edad.php
+++ form/edad.php
@@ -4,32 +4,65 @@
 
 function compare($edad){
 	$result="";
+	$error="";
 	if (empty($edad)){
-		$result= "Error!, you must enter a value";
+		$error= "Error!, you must enter a value";
 	}else{
 		if (is_numeric($edad)){
 			if ( ($edad>=18) && ($edad<=65)){
-				$result="Adulto";
+				$result="Adulto, tiene $edad años";
 			}else{
 				if($edad<18){
-					$result="Joven";
+					$result="Joven, tiene $edad años";
 				}else{
-					$result="Jubilado";
+					$result="Jubilado, tiene $edad años";
 				}
 			}
 		}else{
-			$result= "Error!, you must enter a numeric value";
+			$error= "Error!, you must enter a numeric value";
 		}
 	}
-	return $result;
+	$response= array();
+	if ($error==null){
+		$response=array(
+			"status"=>true,
+			"data"=>$result);
+	}else{
+		$response=array("status"=>false, 
+			"data"=>$error);
+	}
+	return $response;
 }
 //usage
-if ($_SERVER["REQUEST_METHOD"]=="GET"){
+if ($_SERVER["REQUEST_METHOD"]==="GET"){
 		$value=$_GET["edad"];
 	}else{
 		$value= $_POST["edad"];
 	}
 
 $res=compare($value);
-echo $res;
 ?>
+
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div>
+		<select> 
+			<?php for ($i=1;$i>10;$i++){
+				echo "<option>$i</option>";
+			}?>
+		</select>
+	<?php  if ($res["status"]){ ?>
+	Exito: <b><?php  echo $res["data"];?></b>
+	<?php } else { ?>
+	Error: <p style="color:red;">
+		<?php echo $res["data"];?>
+	</p> 
+	<?php } ?> 
+	</div>
+</body>
+</html>
+
+
