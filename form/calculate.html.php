eadc2e54624aa9aa1674339beb26e2016d3a222d
facundoledesma <facundo.ledesma95@gmail.com>
2015-08-06 21:20:24 -0300

    clase3

--- form/calculate.html.php
+++ form/calculate.html.php
@@ -11,5 +11,9 @@
 			<input type="text" value="" name="edad"/>
 			<input type="submit" value="Validate"/>
 		</form>
+		<form action="fibo.html.php" method="GET">
+			<input type="text" value="" name="limite"/>
+			<input type="submit" value="Mostrar Fibo"/>
+		</form>
 </body>
 </html>
